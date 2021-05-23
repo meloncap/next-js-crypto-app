556ce4c014e7e805d22e457d6147ecb0da1fc84c
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-05-23 00:47:10 -0400

    does this work?

--- pages/index.js
+++ pages/index.js
@@ -26,12 +26,13 @@ export default function Home({coins}) {
   )
 }
 
-export const getServerSideProps = async() =>{
+export const getStaticProps = async() =>{
   const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
   const coins = await res.json()
   return{
     props:{
       coins
-    }
+    },
+    revalidate: 5
   }
 }
