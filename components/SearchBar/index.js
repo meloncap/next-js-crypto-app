e0abc1234493076f021fb0093bde1c7bb616ecef
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-05-23 00:07:37 -0400

    fixed search

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,9 +1,9 @@
 import styles from './SearchBar.module.css';
 
-const index = () => {
+const index = ({onChange}) => {
     return (
         <div className={styles.searchBarContainer}> 
-            <input className={styles.searchBar}  type="text" placeholder="ISO 4217 code"/>
+            <input className={styles.searchBar}  type="text" placeholder="ISO 4217 code" onChange={onChange}/>
         </div>
     )
 }
